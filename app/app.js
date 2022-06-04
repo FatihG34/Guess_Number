ccd1272ac300fc07b5a55ed009d05e271945690a
FatihG34 <fthgnydn34@gmail.com>
2022-06-04 20:55:41 +0300

    first changes added

---
+++ app/app.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("#guess").innerHTML
+console.log(input.value);
+const submit = document.querySelector(".guess_label")
+console.log(submit);
+// do {
+
+// } while (condition)
